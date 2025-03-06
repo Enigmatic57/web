d228616c0018968289ba406e2ffc08db98fb6c0d
Enigmatic57 <ahong@eastsideprep.org>
2025-03-06 10:29:06 -0800

    Add files via upload

---
+++ JS Demo/script.js
@@ -0,0 +1,8 @@
+//alert('This is a test')
+
+function askQuestion() {
+    var p = prompt('Why this kolveri di?');
+    if (p != null) {
+        document.getElementById('question').innerHTML = p + "'u";
+    }
+}/Users/avocado/Desktop/midtermproject/css/style.css
